b56fe109285655dd2478a6c40da489cbaa04fa01
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-06-20 10:41:56 +0200

    Added HTML content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+        <Weather />
       <footer>
       This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="blank">Alicia Garcia</a> and is {""}
       <a href="https://github.com/AliOli85/react-weather-app" target="blank">open-sourced on GitHub</a>
