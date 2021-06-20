3d504eb6a4637fc0676fba381009869ced37ae0e
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-06-20 09:57:35 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <h1>Weather App</h1>
+      <footer>
+      This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="blank">Alicia Garcia</a>and is {""}
+      <a href="https://github.com/AliOli85/react-weather-app" target="blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
