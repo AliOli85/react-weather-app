b2c4b6850a56fe17637dc3f63b56f170cbcfe4c5
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-06-20 09:58:19 +0200

    Edited URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-      This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="blank">Alicia Garcia</a>and is {""}
+      This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="blank">Alicia Garcia</a> and is {""}
       <a href="https://github.com/AliOli85/react-weather-app" target="blank">open-sourced on GitHub</a>
       </footer>
     </div>
