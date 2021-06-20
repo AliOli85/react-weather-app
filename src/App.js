1781b7276bffce0e439ac6711c6392e20bb1fdc8
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-06-20 10:14:40 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
       This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="blank">Alicia Garcia</a> and is {""}
       <a href="https://github.com/AliOli85/react-weather-app" target="blank">open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
